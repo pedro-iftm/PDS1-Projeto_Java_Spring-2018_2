b2cde89372da2542faa5b50f96162105eb17dbf8
pedro-iftm <pedroiftm1999@gmail.com>
2018-09-11 22:08:10 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/pedro/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.pedro.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedro.cursomc.domain.Pedido;
+import com.pedro.cursomc.repositories.PedidoRepository;
+import com.pedro.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id: " + id +
+					", tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}
