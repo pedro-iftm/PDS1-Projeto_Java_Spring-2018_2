28f9c0f6d28b304f75d3b44473e599294f9a2a09
pedro-iftm <pedroiftm1999@gmail.com>
2018-10-15 02:57:54 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/pedro/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/pedro/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -17,4 +17,12 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 }
--- src/main/java/com/pedro/cursomc/services/CategoriaService.java
+++ src/main/java/com/pedro/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.pedro.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.pedro.cursomc.domain.Categoria;
 import com.pedro.cursomc.repositories.CategoriaRepository;
+import com.pedro.cursomc.resources.exception.DataIntegrityException;
 import com.pedro.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 	
 }
